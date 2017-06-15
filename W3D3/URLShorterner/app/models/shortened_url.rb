313a85df047cdeac8e97f726ac86bebf76bd6118
App Academy Student <student@appacademy.io>
2017-06-14 17:47:04 -0700

    Add Rails Project

--- W3D3/URLShorterner/app/models/shortened_url.rb
+++ W3D3/URLShorterner/app/models/shortened_url.rb
@@ -6,6 +6,17 @@ class ShortenedUrl < ApplicationRecord
     foreign_key: :user_id,
     class_name: :User
 
+  has_many :visits,
+      primary_key: :id,
+      foreign_key: :shortened_url_id,
+      class_name: :Visit
+
+  has_many :visitors,
+    Proc.new { distinct },
+      through: :visits,
+      source: :users
+
+
   def self.random_code
     found = false
     rand_string = SecureRandom::urlsafe_base64
@@ -17,6 +28,19 @@ class ShortenedUrl < ApplicationRecord
 
   def self.shortener(url, user)
     ShortenedUrl.create(short_url: ShortenedUrl.random_code, long_url: url, user_id: user.id)
+  end
+
+  def num_clicks
+    visits.count
+  end
+
+  def num_uniques
+    visitors.count
+  end
+
+  def num_recent_uniques
+    visits.where({created_at: 10.minutes.ago..Time.now}).select(user_id).distinct.count
+    # count
   end
 
 end
--- W3D3/URLShorterner/db/schema.rb
+++ W3D3/URLShorterner/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614211801) do
+ActiveRecord::Schema.define(version: 20170614221520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 20170614211801) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+  end
+
+  create_table "visits", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "shortened_url_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["shortened_url_id"], name: "index_visits_on_shortened_url_id", using: :btree
+    t.index ["user_id"], name: "index_visits_on_user_id", using: :btree
   end
 
 end
