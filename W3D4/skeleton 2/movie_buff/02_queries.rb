e8b520584aaff392640baac1f15b596ccd68d95d
Noah Kang <noahskang@gmail.com>
2017-06-15 22:10:55 -0700

    Modify movie_buff

--- W3D4/skeleton 2/movie_buff/02_queries.rb
+++ W3D4/skeleton 2/movie_buff/02_queries.rb
@@ -27,6 +27,11 @@ def vanity_projects
   # Show the movie id and title and director's name.
 
   # Note: Directors appear in the 'actors' table.
+  Movies
+    .select("movies.id, movies.title, actors.name")
+    .joins(:actors)
+    .where("director_id = actors.id")
+    .where("castings.ord = 1")
 
 end
 
@@ -34,4 +39,11 @@ def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name and number of supporting roles.
 
+  Actor
+    .select(:id, :name, "COUNT(castings.actor_id) as roles")
+    .joins(:castings)
+    .where.not(castings: {ord: 1})
+    .group("actors.id")
+    .order("roles DESC")
+    .limit(2)
 end
