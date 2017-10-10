54fc51b5cdc879775e655ebdde6ce7413c629a79
Noah Kang <noahskang@gmail.com>
2017-10-10 16:18:11 -0700

    finish github grabber code

--- Node/github-grabber/github_grabber.js
+++ Node/github-grabber/github_grabber.js
@@ -1,31 +1,41 @@
-const fs = require ('fs');
-const qs = require ('querystring');
+const fs = require('fs');
+const qs = require('qs');
 const http = require('http');
 const https = require('https');
 
-const githubServer = http.createServer((req, res)=>{
-  if(req.method === 'POST') {
-    let dataString = '';
-    req.on('data', d => {
-      dataString += d;
+function buildOptionsObj (username) {
+  return {
+    hostname: `api.github.com`,
+    path: `/users/${username}/starred`,
+    headers: {
+      'User-Agent': 'github-grabber'
+    }
+  };
+}
+
+const githubServer = http.createServer((req, res) => {
+  if (req.method === 'POST') {
+    let requestBody = '';
+    req.on('data', chunk => {
+      requestBody += chunk;
     });
-    req.on('end', () => {
-      const username = qs.parse(dataString).username;
-      getGitRepos(username);
+    req.on('end', () =>{
+      const username = qs.parse(requestBody).username;
+      const ws = fs.createWriteStream(`./${username}_starred_repos.txt`);
+      const opts = buildOptionsObj(username);
+      https.get(opts, (dataStream) => {
+        let repoData = '';
+        dataStream.on('data', chunk => { repoData += chunk;  });
+        dataStream.on('end', () => {
+          const repos = JSON.parse(repoData).map(repo => {
+            return `Repo: ${repo.name}. Stars: ${repo.stargazers_count}.`;
+          }).join('\n');
+          ws.write(repos);
+          res.end(repos);
+        });
+      });
     });
-  } else {
-    res.end("Danger, not a POST request!");
   }
 });
 
-const options = {
-  'User-Agent': "noahskang",
-};
-
-const getGitRepos = https.get('https://api.github.com', (res)=>{
-  res.on('data', (d)=>{
-
-  })
-});
-
-githubServer.listen(8080, ()=> console.log('Listening on 8080'));
+githubServer.listen(8080, () => {console.log('Listening on 8080');}); 
